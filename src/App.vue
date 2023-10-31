ea0b0f57eb1566b68c89332e5fc4f69750c65a87
Шустрова <mshustrova@binnopharmgroup.ru>
2023-10-31 14:55:00 +0300

    add App vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+       <v-app>
+         <Navbar />
+         <router-view />
+       </v-app>
+     </template>
+     
+     <script>
+     import Navbar from "./components/Navbar.vue";
+     
+     export default {
+       name: "App",
+       components: {
+         Navbar,
+       },
+     };
+     </script>
